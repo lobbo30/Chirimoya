37c4e046bc067c75f5c19f9cf6a9cea767cd9f78
David <David@DAVID-PC>
2017-06-25 20:18:19 -0500

    Agregar archivos de proyecto.

---
+++ Chirimoya.Tests/GetWeightedEvaluationTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ChirimoyaLib;
+
+namespace Chirimoya.Tests
+{
+    [TestClass]
+    public class GetWeightedEvaluationTests
+    {
+        [TestMethod]
+        public void GetWeightedEvaluation()
+        {
+            List<Alternative> alternatives = new List<Alternative>()
+            {
+                new Alternative() { ID = 1, AlternativeName = "AA CO." },
+                new Alternative() { ID = 2, AlternativeName = "EDS. LTD." },
+                new Alternative() { ID = 3, AlternativeName = "PW. INC." }
+            };
+
+            List<Factor> factors = new List<Factor>()
+            {
+                new Factor() { FactorName = "Salary", Weight = 0.3f, AlternativeValues = new List<AlternativeValue>()
+                {
+                    new AlternativeValue() { FactorEvaluation = 0.7f, Alternative = alternatives[0] },
+                    new AlternativeValue() { FactorEvaluation = 0.8f, Alternative = alternatives[1] },
+                    new AlternativeValue() { FactorEvaluation = 0.9f, Alternative = alternatives[2] }
+                } },
+                new Factor() { FactorName = "Career Advancement", Weight = 0.6f, AlternativeValues = new List<AlternativeValue>()
+                {
+                    new AlternativeValue() { FactorEvaluation = 0.9f, Alternative = alternatives[0] },
+                    new AlternativeValue() { FactorEvaluation = 0.7f, Alternative = alternatives[1] },
+                    new AlternativeValue() { FactorEvaluation = 0.6f, Alternative = alternatives[2] }
+                } },
+                new Factor() { FactorName = "Location", Weight = 0.1f, AlternativeValues = new List<AlternativeValue>()
+                {
+                    new AlternativeValue() { FactorEvaluation = 0.6f, Alternative = alternatives[0] },
+                    new AlternativeValue() { FactorEvaluation = 0.8f, Alternative = alternatives[1] },
+                    new AlternativeValue() { FactorEvaluation = 0.9f, Alternative = alternatives[2] }
+                } }
+            };
+
+            MultifactorEvaluationProcess mep = new MultifactorEvaluationProcess();
+            float resultado = mep.GetWeightedEvaluation(factors, alternatives[0]);
+            float resultado1 = mep.GetWeightedEvaluation(factors, alternatives[1]);
+            float resultado2 = mep.GetWeightedEvaluation(factors, alternatives[2]);
+
+            Assert.AreEqual(0.81f, resultado);
+            Assert.AreEqual(0.74f, resultado1);
+            Assert.AreEqual(0.72f, resultado2);
+        }
+    }
+}
---
+++ ChirimoyaLib/Alternative.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChirimoyaLib
+{
+    public class Alternative
+    {
+        public int ID { get; set; }
+        public string AlternativeName { get; set; }
+        public List<AlternativeValue> AlternativeValues { get; set; }
+    }
+
+    public class AlternativeValue
+    {
+        public float FactorEvaluation { get; set; }
+        public Alternative Alternative { get; set; }
+        public Factor Factor { get; set; }
+    }
+
+    public class Factor
+    {
+        public string FactorName { get; set; }
+        public float Weight { get; set; }
+        public List<AlternativeValue> AlternativeValues { get; set; }
+
+        public Factor()
+        {
+        
+        }
+    }
+}
