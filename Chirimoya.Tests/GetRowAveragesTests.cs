cc282a79464d9815b14a88cbe5ea14772a4c1aea
David Quequezana <david.quequezana@outlook.com>
2017-06-25 22:18:04 -0500

    GetRowAverages tests have been added

---
+++ Chirimoya.Tests/GetRowAveragesTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChirimoyaLib;
+
+namespace Chirimoya.Tests
+{
+    [TestClass]
+    public class GetRowAveragesTests
+    {
+        [TestMethod]
+        public void GetRowAverages()
+        {
+            float[,] temp = new float[3, 3];
+            temp[0, 1] = 3f;
+            temp[0, 2] = 9f;
+            temp[1, 2] = 6f;
+
+            float[] resultado = AnalyticHierarchyProcess.GetRowAverages(temp);
+
+            Assert.AreEqual(0.6582692f, resultado[0]);
+            Assert.AreEqual(0.2819231f, resultado[1]);
+            Assert.AreEqual(0.05980769f, resultado[2]);
+        }
+
+        [TestMethod]
+        public void GetRowAverages_WithDifferentArguments()
+        {
+            float[,] temp = new float[3, 3];
+            temp[0, 1] = 4f;
+            temp[0, 2] = 6f;
+            temp[1, 2] = 5f;
+
+            float[] resultado = AnalyticHierarchyProcess.GetRowAverages(temp);
+
+            Assert.AreEqual(0.658371041f, resultado[0]);
+            Assert.AreEqual(0.261814982f, resultado[1]);
+            Assert.AreEqual(0.079813977f, resultado[2]);
+        }
+    }
+}
---
+++ ChirimoyaLib/AnalyticHierarchyProcess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ChirimoyaLib
+{
+    public class AnalyticHierarchyProcess
+    {
+        public static float[] GetRowAverages(float[,] temp)
+        {
+            temp[0, 0] = 1f;
+            temp[1, 1] = 1f;
+            temp[2, 2] = 1f;
+
+            temp[1, 0] = 1f / temp[0, 1];
+            temp[2, 0] = 1f / temp[0, 2];
+            temp[2, 1] = 1f / temp[1, 2];
+
+            float[] columnsTotals = new float[3];
+            for (int n = 0; n < 3; n++)
+            {
+                float suma = 0f;
+                for (int m = 0; m < 3; m++)
+                {
+                    suma += temp[m, n];
+                }
+                columnsTotals[n] = suma;
+            }
+
+            float[,] matrixNormalized = new float[3, 3];
+            for (int m = 0; m < temp.GetLength(0); m++)
+            {
+                for (int n = 0; n < temp.GetLength(1); n++)
+                {
+                    matrixNormalized[m, n] = temp[m, n] / columnsTotals[n];
+                }
+            }
+
+            float[] rowAverages = new float[3];
+            for (int m = 0; m < 3; m++)
+            {
+                float suma = 0f;
+                for (int n = 0; n < 3; n++)
+                {
+                    suma += matrixNormalized[m, n];
+                }
+                rowAverages[m] = suma / 3f;
+            }
+
+            return rowAverages;
+        }
+    }
+}
